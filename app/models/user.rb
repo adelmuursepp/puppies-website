c307ec09360765ab6cd8e35ec2bd25efb557fba7
Adel Muursepp <adel.muursepp@gmail.com>
2019-07-15 15:35:29 +0300

    took out the validates name from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  validates :name, presence: true, allow_blank: false
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
