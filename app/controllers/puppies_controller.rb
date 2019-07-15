e5631c0e451c1a0184bd48db68b110fbeb7908f4
Adel Muursepp <adel.muursepp@gmail.com>
2019-07-15 20:06:46 +0300

    Created the base for you posted a puppy listing

--- app/controllers/puppies_controller.rb
+++ app/controllers/puppies_controller.rb
@@ -1,20 +1,28 @@
 class PuppiesController < ApplicationController
+  before_action :set_puppy, only: [ :show, :listing ]
+
   def new
     @puppy = Puppy.new
   end
 
   def show
-    @puppy = Puppy.find(params[:id])
   end
 
   def create
     @puppy = Puppy.new(puppy_attributes)
     @puppy.user = current_user
     if @puppy.save
-      redirect_to listing_puppy_path(1)
+      redirect_to listing_puppy_path(@puppy.id)
     else
       render :new
     end
+  end
+
+  def listing
+  end
+
+  def set_puppy
+    @puppy = Puppy.find(params["id"])
   end
 
   def puppy_attributes
