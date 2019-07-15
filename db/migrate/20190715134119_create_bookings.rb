5f23db220f2c90e5b9ebef75b5ae5b8bdea45305
Adel Muursepp <adel.muursepp@gmail.com>
2019-07-15 16:44:04 +0300

    bookings model and controller

---
+++ db/migrate/20190715134119_create_bookings.rb
@@ -0,0 +1,12 @@
+class CreateBookings < ActiveRecord::Migration[5.2]
+  def change
+    create_table :bookings do |t|
+      t.date :start_date
+      t.date :end_date
+      t.references :dog, foreign_key: true
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
