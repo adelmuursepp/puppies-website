66fe90d6555ac7b992540860e3703434be6107fb
Adel Muursepp <adel.muursepp@gmail.com>
2019-07-15 16:07:02 +0300

    puppy model added, without controller and validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_15_120957) do
+ActiveRecord::Schema.define(version: 2019_07_15_123824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "puppies", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.string "breed"
+    t.string "photo"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_puppies_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +40,5 @@ ActiveRecord::Schema.define(version: 2019_07_15_120957) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "puppies", "users"
 end
---
+++ test/models/puppy_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PuppyTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
