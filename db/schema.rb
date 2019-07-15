7d9cac102345684c29419abf55601864662c99fa
Adel Muursepp <adel.muursepp@gmail.com>
2019-07-15 16:58:12 +0300

    fixed db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_15_123824) do
+ActiveRecord::Schema.define(version: 2019_07_15_134119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "puppy_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["puppy_id"], name: "index_bookings_on_puppy_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "puppies", force: :cascade do |t|
     t.string "name"
@@ -40,5 +51,7 @@ ActiveRecord::Schema.define(version: 2019_07_15_123824) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "puppies"
+  add_foreign_key "bookings", "users"
   add_foreign_key "puppies", "users"
 end
