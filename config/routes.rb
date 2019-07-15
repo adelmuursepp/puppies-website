449582bbd47e326e8f37cfd1d9af81cf51aa1e66
Alfredo Perez <alfredopzr1@gmail.com>
2019-07-15 18:14:33 +0200

    Fixing problems

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
     member do
       get 'listing', to: 'puppies#listing'
-      get 'show', to: 'puppies#show'
+      get '/', to: 'puppies#show', as: ''
     end
   end
 
