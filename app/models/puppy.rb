88951aed44cc932f27465c5d1b36e5bb8359f4e7
Adel Muursepp <adel.muursepp@gmail.com>
2019-07-17 16:15:35 +0300

    Lord and commander at it again on master

--- app/models/puppy.rb
+++ app/models/puppy.rb
@@ -4,7 +4,7 @@ class Puppy < ApplicationRecord
   has_many :takers, through: :bookings, source: :user
   has_many :givers, through: :bookings
   mount_uploader :photo, PhotoUploader
-  # geocoded_by :address
+  geocoded_by :address
   # after_validation :geocode, if: :will_save_change_to_address?
   validates :name, presence: true, allow_blank: true
   validates :age, presence: true, allow_blank: false, numericality: { only_integer: true, less_than: 15 }
