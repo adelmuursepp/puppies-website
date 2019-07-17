147b1b76eef06619211d3ad3c334a6cd44ae1ff9
Adel Muursepp <adel.muursepp@gmail.com>
2019-07-17 16:18:38 +0300

    Ollies last fix

--- app/models/puppy.rb
+++ app/models/puppy.rb
@@ -5,7 +5,7 @@ class Puppy < ApplicationRecord
   has_many :givers, through: :bookings
   mount_uploader :photo, PhotoUploader
   geocoded_by :address
-  # after_validation :geocode, if: :will_save_change_to_address?
+  after_validation :geocode, if: :will_save_change_to_address?
   validates :name, presence: true, allow_blank: true
   validates :age, presence: true, allow_blank: false, numericality: { only_integer: true, less_than: 15 }
   validates :breed, presence: true, allow_blank: false
