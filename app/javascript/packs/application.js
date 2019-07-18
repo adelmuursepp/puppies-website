6fa1b869bddd6cefc2619c204df5c41b3836326d
(Simon Martinez) <smartineze1993@gmail.com>
2019-07-18 16:24:56 +0200

    add sutocomplete feature to address when creating a puppy

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,5 +3,7 @@ import "jquery";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 import { initMapbox } from '../plugins/init_mapbox.js';
+import { initAutocomplete } from '../plugins/init_autocomplete.js';
 initUpdateNavbarOnScroll();
 initMapbox();
+initAutocomplete();
--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -2,8 +2,7 @@ import places from 'places.js';
 
 const initAutocomplete = () => {
 
-  const addressInput = document.getElementById('search_query');
-  console.log(addressInput)
+  const addressInput = document.getElementById('puppy_address');
   if (addressInput) {
     places({ container: addressInput, type: ['city', 'address'] });
   }
