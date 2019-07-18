edb7e26fa83047bb76d079ee72661a9e57231715
Adel Muursepp <adel.muursepp@gmail.com>
2019-07-18 13:25:01 +0300

    now you can delete puppies

--- app/models/puppy.rb
+++ app/models/puppy.rb
@@ -1,6 +1,6 @@
 class Puppy < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :takers, through: :bookings, source: :user
   has_many :givers, through: :bookings
   mount_uploader :photo, PhotoUploader
